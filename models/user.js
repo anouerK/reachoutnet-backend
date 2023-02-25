9cdc4cefa914c2bdfd7f23962507635983223fcd
ksaay2000 <anoueramin.kassaa@esprit.tn>
2023-02-25 23:38:15 +0100

    package update + user changes

--- models/user.js
+++ models/user.js
@@ -4,8 +4,10 @@ var schema = mongoose.Schema;
 mongoose.set("strictQuery", false);
 
 var User = new schema({
-  name: String,
-  age: Number
+  username: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['user', 'admin'], default: 'user' },
 });
 //db name in lowercase only
 module.exports = mongoose.model("users", User);
