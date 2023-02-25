04b1996fe0e251fee326888da62464387196d359
ksaay2000 <anoueramin.kassaa@esprit.tn>
2023-02-26 00:49:51 +0100

    user cruds

--- models/gestion_user/user.js
+++ models/gestion_user/user.js
@@ -5,9 +5,15 @@ mongoose.set("strictQuery", false);
 
 var User = new schema({
   username: { type: String, required: true },
+  first_name: { type: String, required: true },
+  last_name: { type: String, required: true },
+  age: { type: Number, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['user', 'admin'], default: 'user' },
+  role: { type: String, enum: ["user", "staff","admin"], default: "user" },
+  creation_date: { type: Date,default: Date.now },
+  last_login: { type: Date },
+  //active: { type: Date },
 });
 //db name in lowercase only
 module.exports = mongoose.model("users", User);
