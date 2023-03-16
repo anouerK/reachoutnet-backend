85f4df778ab298828e609e00e586379d28fbcbc9
ksaay2000 <anoueramin.kassaa@esprit.tn>
2023-03-16 05:35:21 +0100

    implemented linkedin login methode

--- index.js
+++ index.js
@@ -9,7 +9,32 @@ require("dotenv").config();
 const typeDefs = gql(readFileSync("./user.graphql", { encoding: "utf-8" }));
 const resolvers = require("./resolvers");
 const UserAPI = require("./datasources/UserApi");
+/*
+const https = require("https");
 
+const options = {
+    hostname: "www.linkedin.com",
+    path: "/oauth/v2/accessToken?code=AQRZQjbbiGs2fIJA8o0UqBR27y3MRmTOqifhKpm0hrkHcrt1WW1moBX53KPK02mOwRKMcd09VxiDtUPNM0-dgiOtCN1ecvw0Df0BgzBiGvAEyIZRFcssjtIGBjR1eOMe4OW5s8_DFGlioy_dFv8f4bf_afiKPdO0LcKaJdA9wHM5IuljKqX_Q3xXgFuIpB_yyRK7_Fr4ITry0OLIIyM&grant_type=authorization_code&client_id=783i64m5ssj1yr&client_secret=SFlxPWAtQfcytNMR&redirect_uri=http://localhost:3001/linkedin",
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+};
+
+const req = https.request(options, res => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on("data", d => {
+        process.stdout.write(d);
+    });
+});
+
+req.on("error", error => {
+    console.error(error);
+});
+
+req.end();
+*/
 async function startApolloServer () {
     // const app = express();
     const server = new ApolloServer({
