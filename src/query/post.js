69e0b9d290b9a0f452f29b27e2567d919e3dedff
ksaay2000 <anoueramin.kassaa@esprit.tn>
2023-04-17 19:10:56 +0100

    posts per user  / attachements

--- src/query/post.js
+++ src/query/post.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-unused-vars */
 const { isauthenticated, userpermission, authorize } = require("../../middleware/userpermission");
+const { GraphQLError } = require("graphql");
 const post_query = {
     users_posts: async (_, __, { dataSources, req }) => {
         const posts = await dataSources.postAPI.findPostsandAuthors("users");
@@ -9,6 +10,17 @@ const post_query = {
     get_user_posts: async (_, { __ }, { dataSources, req }) => {
         const user = await isauthenticated()(req);
         const posts = await dataSources.postAPI.findPostsbyAuthor("users", user._id);
+        return posts;
+    },
+    get_user_posts_by_id: async (_, { id }, { dataSources, req }) => {
+        await isauthenticated()(req);
+
+        const user = await dataSources.userAPI.findOnebyId(id);
+
+        if (!user) { throw new GraphQLError("User Not FOUND."); }
+
+        const posts = await dataSources.postAPI.findPostsbyAuthor("users", id);
+        console.log(posts);
         return posts;
     },
     post: async (_, { id }, { dataSources, req }) => {
